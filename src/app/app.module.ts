4c145cafa972668651d09319be4422670b97916a
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-07-05 18:23:00 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,30 @@ import { AppComponent } from './app.component';
 import { MediHerbalComponent } from './Herbal/medi-herbal/medi-herbal.component';
 import { SmileStoreComponent } from './Modern/smile-store/smile-store.component';
 import { REximComponent } from './import-Export/r-exim/r-exim.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ClubComponent } from './modern/club/club.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MediHerbalComponent,
     SmileStoreComponent,
-    REximComponent
+    REximComponent,
+    ClubComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    FlexLayoutModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
